b43d7f13f6eabb6947281a4f206f1f07c9e2f15c
Arshpreet Singh <arshsomal100@gmail.com>
2025-07-15 23:27:24 +0530

    fixed cors error

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 import authRoutes from "./routes/auth.routes";
